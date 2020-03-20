65e2b8f90632eaf96bf1ba2cdacefca927ae7a66
PecaLee <58648931+PecaLee@users.noreply.github.com>
2020-03-20 19:49:53 +0900

    Update bingo5gui.py

--- bingo5gui.py
+++ bingo5gui.py
@@ -1,8 +1,14 @@
 import tkinter
+import tkinter.font as tkFont
 
 bingo_window = tkinter.Tk()
+font_style = tkFont.Font(size = 20)
 
 bingo_window.title("bingo5 抽選機")
 bingo_window.geometry("400x400")
+bingo_window.resizable(False, True)
+
+title = tkinter.Label(bingo_window, text = "bingo5 抽選機", height = 5, font = font_style)
+title.pack()
 
 bingo_window.mainloop()
